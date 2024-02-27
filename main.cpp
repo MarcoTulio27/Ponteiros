7f9408949cdaac9dc1143b134f2d18c24718f143
Marco Tulio Silva de Carvalho <114110670+MarcoTulio27@users.noreply.github.com>
2024-02-27 12:12:36 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,26 +2,41 @@
 
 using namespace std;
 
+float prodEscalar(int n, float x[], float y[]){
+    float soma = 0;
+    for(int i =0; i<n; i++){
+        soma+= x[i] * y[i];
+
+    }
+    return soma;
+}
+
 int main()
 {
     int n;
     cout << "Tamanho do vetor: ";
     cin >> n;
 
+    float *vet1 = new float[n];
+    float *vet2 = new float[n];
 
-    int *vet = new int[n];
-    cout << "Elementos do vetor: " << endl;
+    cout << "Elementos do vetor1: ";
     for(int i =0; i<n; i++){
-        cin >> vet[i];
+        cin >> vet1[i];
     }
-    float soma = 0;
-    for(int i =0; i<n; i++){
-        soma+=vet[i];
-    }
-    float media = soma/n;
-    cout << "Media dos elementos = " << media << endl;
 
-    delete [] vet;
+    cout << "Elementos do vetor2: ";
+    for(int i =0; i<n; i++){
+        cin >> vet2[i];
+    }
+
+    float produto;
+
+    produto = prodEscalar(n, vet1, vet2);
+    cout << "Produto escalar dos vetores: " << produto;
+
+    delete [] vet1;
+    delete [] vet2;
 
     return 0;
 }
